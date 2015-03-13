aee1cc873e401410fb78399043793141ac0eb8f2
Jason Madsen <jmadsen@instructure.com>
2015-03-12 20:16:15 -0600

    run rake routes when no path supplied

--- lib/routler/consumer.rb
+++ lib/routler/consumer.rb
@@ -2,21 +2,16 @@ module Routler
   class Consumer
 
     def load_file path=nil
-      if path
-        contents = IO.read(path)
-      else
-        build_routes
-      end
+      path ? IO.read(path) : build_routes
     end
 
     def build_routes
-      # TODO
-      # contents = ''
-      # pipe = IO.popen("bundle exec rails routes")
-      # while (line = pipe.gets)
-      #   contents += line
-      # end
-      # contents
+      contents = ''
+      pipe = IO.popen("bundle exec rake routes")
+      while (line = pipe.gets)
+        contents += line
+      end
+      contents
     end
 
   end
